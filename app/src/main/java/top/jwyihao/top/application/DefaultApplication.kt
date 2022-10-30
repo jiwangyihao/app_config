741111d33f25c94e59c8daf043ca311bfdd02c00
jiwangyihao <jwyh@jwyihao.top>
2022-10-30 16:43:07 +0800

    初始化

---
+++ app/src/main/java/top/jwyihao/top/application/DefaultApplication.kt
@@ -0,0 +1,11 @@
+package top.jwyihao.top.application
+
+import com.highcapable.yukihookapi.hook.xposed.application.ModuleApplication
+
+class DefaultApplication : ModuleApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+        // Your code here.
+    }
+}
---
+++ app/src/main/java/top/jwyihao/top/hook/HookEntry.kt
@@ -0,0 +1,18 @@
+package top.jwyihao.top.hook
+
+import com.highcapable.yukihookapi.annotation.xposed.InjectYukiHookWithXposed
+import com.highcapable.yukihookapi.hook.factory.configs
+import com.highcapable.yukihookapi.hook.factory.encase
+import com.highcapable.yukihookapi.hook.xposed.proxy.IYukiHookXposedInit
+
+@InjectYukiHookWithXposed
+class HookEntry : IYukiHookXposedInit {
+
+    override fun onInit() = configs {
+        // Your code here.
+    }
+
+    override fun onHook() = encase {
+        // Your code here.
+    }
+}
