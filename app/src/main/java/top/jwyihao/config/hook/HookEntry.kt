3ee7adb1dc22c4410445d55b66222c2255689d8d
jwyh@jwyihao.top <jwyh@jwyihao.top>
2022-11-03 07:23:00 +0800

    .

--- app/src/main/java/top/jwyihao/config/hook/HookEntry.kt
+++ app/src/main/java/top/jwyihao/config/hook/HookEntry.kt
@@ -14,5 +14,24 @@ class HookEntry : IYukiHookXposedInit {
 
     override fun onHook() = encase {
         // Your code here.
+        loadZygote {
+            ActivityClass.hook { 
+                injectMember { 
+                    method { 
+                        name = "onCreate"
+                        param(BundleClass)
+                        returnType = UnitType
+                    }
+                    afterHook {
+                        // Your code here.
+                        AlertDialog.Builder(instance())
+                                            .setTitle("Hooked")
+                                            .setMessage("I am hook!")
+                                            .setPositiveButton("OK", null)
+                                            .show()
+                    }
+                }
+            }
+        }
     }
 }
