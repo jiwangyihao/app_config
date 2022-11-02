098ce8a0c6a817c80a03bfa7952caeefe3c82ad2
jwyh@jwyihao.top <jwyh@jwyihao.top>
2022-11-03 00:27:54 +0800

    .

--- app/src/androidTest/java/top/jwyihao/config/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/top/jwyihao/config/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("top.jwyihao.top", appContext.packageName)
+        assertEquals("top.jwyihao.configfig", appContext.packageName)
     }
 }
