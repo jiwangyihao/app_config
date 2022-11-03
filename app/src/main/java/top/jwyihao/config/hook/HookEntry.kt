afc8f47dab1973d6650fc21d01bb4b0e33e5803e
jwyh@jwyihao.top <jwyh@jwyihao.top>
2022-11-03 12:18:18 +0800

    .

--- app/src/main/java/top/jwyihao/config/hook/HookEntry.kt
+++ app/src/main/java/top/jwyihao/config/hook/HookEntry.kt
@@ -1,6 +1,25 @@
 package top.jwyihao.config.hook
 
-import com.highcapable.yukihookapi.*
+import android.app.Activity
+import android.content.Intent
+import android.widget.Button
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.highcapable.yukihookapi.YukiHookAPI
+import com.highcapable.yukihookapi.annotation.xposed.InjectYukiHookWithXposed
+import com.highcapable.yukihookapi.demo_module.R
+import com.highcapable.yukihookapi.demo_module.data.DataConst
+import com.highcapable.yukihookapi.demo_module.hook.factory.compatStyle
+import com.highcapable.yukihookapi.demo_module.ui.MainActivity
+import com.highcapable.yukihookapi.hook.factory.applyModuleTheme
+import com.highcapable.yukihookapi.hook.factory.registerModuleAppActivities
+import com.highcapable.yukihookapi.hook.type.android.ActivityClass
+import com.highcapable.yukihookapi.hook.type.android.BundleClass
+import com.highcapable.yukihookapi.hook.type.java.StringArrayClass
+import com.highcapable.yukihookapi.hook.type.java.StringType
+import com.highcapable.yukihookapi.hook.type.java.UnitType
+import com.highcapable.yukihookapi.hook.xposed.bridge.event.YukiXposedEvent
+import com.highcapable.yukihookapi.hook.xposed.proxy.IYukiHookXposedInit
+
 
 @InjectYukiHookWithXposed
 class HookEntry : IYukiHookXposedInit {
