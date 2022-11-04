34295bd689729f10b871badf7002d1a11c84f052
jwyh@jwyihao.top <jwyh@jwyihao.top>
2022-11-04 13:42:17 +0800

    .

--- app/src/main/java/top/jwyihao/config/hook/HookEntry.kt
+++ app/src/main/java/top/jwyihao/config/hook/HookEntry.kt
@@ -3,6 +3,7 @@ package top.jwyihao.config.hook
 import android.app.Activity
 import android.content.res.Configuration
 import android.content.res.Resources
+import android.util.DisplayMetrics
 import android.content.Intent
 import android.widget.Button
 import android.app.AlertDialog
@@ -19,6 +20,7 @@ import com.highcapable.yukihookapi.hook.factory.current
 import com.highcapable.yukihookapi.hook.type.android.ActivityClass
 import com.highcapable.yukihookapi.hook.type.android.ResourcesClass
 import com.highcapable.yukihookapi.hook.type.android.ConfigurationClass
+import com.highcapable.yukihookapi.hook.type.android.DisplayMetricsClass
 import com.highcapable.yukihookapi.hook.type.java.StringArrayClass
 import com.highcapable.yukihookapi.hook.type.java.StringType
 import com.highcapable.yukihookapi.hook.type.java.UnitType
@@ -30,28 +32,46 @@ import java.lang.reflect.Field
 @InjectYukiHookWithXposed
 class HookEntry : IYukiHookXposedInit {
 
-    override fun onInit() = configs {
-        // Your code here.
-    }
+  override fun onInit() = configs {
+      // Your code here.
+  }
 
-    override fun onHook() = encase {
-        // Your code here.
-        loadZygote {
-          ResourcesClass.hook {
-            injectMember {
-              method { 
-                name = "updateConfiguration"
-                param(ConfigurationClass)
-              }
-              beforeHook {
-                // Your code here.
-                Toast.makeText(appContext, "『应用配置』运行中",Toast.LENGTH_SHORT).show();
-                var configuration: Configuration? = Configuration(args().first().cast<Configuration?>())
-                configuration?.current()?.field { name = "densityDpi" }?.set(320)
-                args().first().set(configuration)
+  override fun onHook() = encase {
+    // Your code here.
+    loadZygote {
+      ResourcesClass.hook {
+        injectMember {
+          method { 
+            name = "updateConfiguration"
+            param(ConfigurationClass,DisplayMetricsClass)
+          }
+          beforeHook {
+            // Your code here.
+            Toast.makeText(appContext, "『应用配置』运行中",Toast.LENGTH_SHORT).show();
+
+            var configuration: Configuration? = Configuration(args().first().cast<Configuration?>())
+            val dpi = 320
+
+            var displayMetrics: DisplayMetrics? = null
+            if (args().last().cast<DisplayMetrics?>() != null) {
+              displayMetrics = DisplayMetrics()
+              displayMetrics.setTo(args().last().cast<DisplayMetrics?>())
+              args().last().set(displayMetrics)
+            } else {
+              displayMetrics = instance<Resources>().displayMetrics
+            }
+            if (displayMetrics != null) {
+              displayMetrics.density = dpi / 160f
+              displayMetrics.densityDpi = dpi
+              if (Build.VERSION.SDK_INT >= 17) {
+                configuration?.current()?.field { name = "densityDpi" }?.set(dpi)
               }
             }
+            
+            args().first().set(configuration)
           }
         }
+      }
     }
+  }
 }
