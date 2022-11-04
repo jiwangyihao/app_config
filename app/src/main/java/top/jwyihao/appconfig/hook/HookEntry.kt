aee3e0330c83a7245dad1dd519b43d324246f64b
jwyh@jwyihao.top <jwyh@jwyihao.top>
2022-11-04 22:40:44 +0800

    y

--- app/src/main/java/top/jwyihao/appconfig/hook/HookEntry.kt
+++ app/src/main/java/top/jwyihao/appconfig/hook/HookEntry.kt
@@ -55,7 +55,7 @@ class HookEntry : IYukiHookXposedInit {
             val dpi: Int = 320
             if (dpi > 0) {
               // Density for this package is overridden, change density
-              instance.current()?.field { name = "mDisplayInfo" }.get()
+              instance.current()?.field { name = "mDisplayInfo" }?.get().any()
                 .current()?.field { name = "logicalDensityDpi" }?.set(dpi)
             }
           }
